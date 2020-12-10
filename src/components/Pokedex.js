c1eafa907c2ece1e8da388da9ba38e2e7a5701c3
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 16:52:36 -0500

    [img] Set image off with p tags

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 function Pokedex() {
     const [pokemonName, setPokemonName] = useState('pikachu')
     const [pokemonImage, setPokemonImage] = useState('')
     
+    useEffect(() => {
+        console.log('IN USEEFFECT')
+        if (pokemonName === '') {
+            return
+        }
+        Axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then((res) => {
+                setPokemonImage(res.data.sprites.front_default)
+            })
+    })
+
     return(
         <div>
             <h1>Fischer-Price My First Pokedex</h1>
             <input value={pokemonName} onChange={(event) => {
-                setPokemonName
+                setPokemonName(event.target.value.toLowerCase())
             }} />
-            <img src={pokemonImage} />
+            <p><img src={pokemonImage} /></p>
         </div>
     )
 }
